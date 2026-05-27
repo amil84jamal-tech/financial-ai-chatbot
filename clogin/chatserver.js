1eb778bab08ad196df63ebefc2df97351e5ab05a
Amil Jamal <amilaminjamal@Amils-MacBook-Air.local>
2026-05-28 01:37:24 +0530

    Completed Registration Authentication

--- clogin/chatserver.js
+++ clogin/chatserver.js
@@ -2,8 +2,19 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 4000
 const con = require('./dbConfig');
+const bcrypt = require('bcrypt')
+const session = require('express-session');
+const flash = require("express-flash");
 
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({extended: true}));
+app.use(session({
+    secret : "secret",
+    resave : false,
+    saveUninitialised: false
+}));
+app.use(flash());
 
 app.get('/',(req,res)=>{
     res.render("index")
@@ -20,6 +31,66 @@ app.get('/users/register',(req,res)=>{
 app.get('/users/dashboard',(req,res)=>{
     res.render("dashboard")
 })
+
+app.post('/users/register', async(req,res)=>{
+
+    const {name,email,password, password2} = req.body;
+    console.log(name,email,password,password2);
+
+    let errors = [];
+    if(!name || !email || !password || !password2)
+    {
+        errors.push({message: "Enter all entries"});
+    }
+
+    if(password.length < 6)
+    {
+        errors.push({message : "Password should contain atleast 6 charachters"});
+    }
+    
+    if(password != password2)
+    {
+        errors.push({message : "Password Incorrect"});
+    }
+    
+    if(errors.length > 0)
+    {
+        res.render("register", { errors });
+    }
+
+    else
+    {
+        const hashedPassword = await bcrypt.hash(password,10);
+        console.log(hashedPassword);
+
+        con.query("SELECT * FROM cuser WHERE email=$1 ", [email], (err,result)=>{
+            if(err)
+            {
+                throw err;
+            }
+            else
+            {
+                  
+            
+                con.query("INSERT INTO cuser(name,email,password) VALUES($1,$2,$3) RETURNING id,password",[name,email,hashedPassword], (err,result)=>{
+                    if(err)
+                    {
+                        throw err;
+                    }
+
+                    else
+                    {
+                        req.flash('success_msg',"Account successfully created");
+                        res.redirect("/users/login");
+                    }
+                })
+            }
+            }    
+            
+        )
+    }
+}
+)
 
 app.listen(PORT,()=>{
     console.log("Server Connected.....")
